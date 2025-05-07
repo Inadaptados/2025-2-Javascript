f335ed0e0a8e4037100d39db80be00e8ae12424e
Fernando <fernando.acuna@aguascalientes.gob.mx>
2025-05-07 12:25:45 -0600

    tarea 2

--- semana1/clase1_tarea.js
+++ semana1/clase1_tarea.js
@@ -6,10 +6,14 @@ async function main() {
   const num1 = Number(await ask('Ingresa el primer número: '));
   const num2 = Number(await ask('Ingresa el segundo número: '));
 
-  const suma = num1 + num2;
+  const suma = num1 + num2; resta = num1 - num2; multiplicacion = num1 * num2; division = num1 / num2; potencia = num1 ** num2; // TODO: Implementar la potencia
   // TODO: Implementar la resta, multiplicación y división
 
+  console.log('Resta:', resta);
+  console.log('Multiplicación:', multiplicacion);
+  console.log('División:', division);
   console.log('Suma:', suma);
+  console.log('Potencia:', potencia);
   // TODO: Mostrar el resultado de la resta, multiplicación y división
 
 }
