81624466fc8a80164aa33ba3359f84833baaee3b
Fernando <fernando.acuna@aguascalientes.gob.mx>
2025-05-22 14:02:16 -0600

    x

--- semana3/clase1_tarea.js
+++ semana3/clase1_tarea.js
@@ -15,3 +15,27 @@
 // El promedio general de calificaciones.
 // La calificación mayor.
 // La calificación menor.
+
+const { ask } = require('../helpers/input');
+
+async function main() {
+  const alumnos =[
+    {
+    nombre:"Luis",
+    edad:"25",
+    calificacion:"67"
+  },
+  {
+    nombre:"Pedro",
+    edad:"56",
+    calificacion:"87"
+  },
+
+  ]  
+  while(opcion !==3)
+    
+
+  
+}
+
+main();
